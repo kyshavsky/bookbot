d4577c0290df03c9b1a651745219e782163d2538
kyshavsky <kyle.varshavsky@gmail.com>
2025-12-04 21:17:04 +1100

    Erase test print, added book reading function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(book_path):
+    with open(book_path) as f:
+        file_contents = f.read()
+    print(file_contents)
+    
+
+
+get_book_text("/home/kylev/workspace/github.com/kylev/bookbot/books/frankenstein.txt")
+
