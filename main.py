b3485e881b626e22a818b408b097e4381efd486f
kyshavsky <kyle.varshavsky@gmail.com>
2025-12-06 16:25:56 +1100

    counting words function created and successful

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-def get_book_text(book_path):
+# def get_book_text(book_path):
+#     with open(book_path) as f:
+#         file_contents = f.read()
+#     print(file_contents)
+    
+
+def count_words(book_path):
+    num_words = 0
     with open(book_path) as f:
         file_contents = f.read()
-    print(file_contents)
+        word_split = file_contents.split()
+        #print(word_split)
+    for word in word_split:
+        num_words +=1
+    print(f"Found {num_words} total words")
+
+
     
 
 
-get_book_text("/home/kylev/workspace/github.com/kylev/bookbot/books/frankenstein.txt")
+
+
+count_words("/home/kylev/workspace/github.com/kylev/bookbot/books/frankenstein.txt")
 
