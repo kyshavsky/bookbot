03c66b35f7bb9c3c25c65912fc7fb9886871b25a
kyshavsky <kyle.varshavsky@gmail.com>
2025-12-06 17:26:08 +1100

    refactored and created stats.py for number crunching code

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-# def get_book_text(book_path):
-#     with open(book_path) as f:
-#         file_contents = f.read()
-#     print(file_contents)
-    
+from stats import count_words
+#Function pulls book text from txt file
 
-def count_words(book_path):
-    num_words = 0
-    with open(book_path) as f:
-        file_contents = f.read()
-        word_split = file_contents.split()
-        #print(word_split)
-    for word in word_split:
-        num_words +=1
+def get_book_text(book_path): #funciton name
+    with open(book_path) as f: #finds book path and opens file
+        file_contents = f.read() #reads content file and saves to a new variable
+    return file_contents #prints contents of file
+
+def main():
+    book_path = "/home/kylev/workspace/github.com/kylev/bookbot/books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
     print(f"Found {num_words} total words")
 
+main()
+
 
     
 
 
 
 
-count_words("/home/kylev/workspace/github.com/kylev/bookbot/books/frankenstein.txt")
+
 
---
+++ stats.py
@@ -0,0 +1,12 @@
+def count_words(text):
+    num_words = 0
+    for word in text.split():
+        num_words +=1
+    return num_words
+
+    
+    
+    
+    
+   
+
