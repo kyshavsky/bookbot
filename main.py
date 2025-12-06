3bab2e8d4257ced459087f7efa0eb95c459fa251
kyshavsky <kyle.varshavsky@gmail.com>
2025-12-06 20:08:53 +1100

    making progress?

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_char
+from stats import count_words, count_char, chars_dict_to_sorted_list
 #Function pulls book text from txt file
 
 def get_book_text(book_path): #funciton name
@@ -9,10 +9,23 @@ def get_book_text(book_path): #funciton name
 def main():
     book_path = "/home/kylev/workspace/github.com/kylev/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
+    
     num_words = count_words(text)
     print(f"Found {num_words} total words")
+    
     chars = count_char(text)
-    print(chars)
+    sorted_chars = chars_dict_to_sorted_list(chars)
+
+    for item in sorted_chars:
+        char = item["char"]
+        if not char.isaplha():
+            continue
+        num =item["num"]
+        print(f"{char}: {num}")
+
+
+    
+
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -14,10 +14,38 @@ def count_char(text):
         else:
             char_dict[char] = 1
     return char_dict
+
+
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_char_dict):
+   final_list = []
+   for k,v in num_char_dict.items():
+        small_dict = {"char": k, "num": v}
+        final_list.append(small_dict)
+
+   final_list.sort(reverse=True, key=sort_on)
+   return final_list
+   
+
+
+    
+   
+
+
+
+
+
+
+
+
     
    
 # if __name__ == "__main__":
-#     result = count_char("hello") 
+#     dict = count_char("hello I am a person I love pizza") 
+#     result = chars_dict_to_sorted_list(dict)
 #     print(result)
    
 
